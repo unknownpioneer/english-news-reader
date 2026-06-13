4d37663496011e9e1c9b0592d04c48eb4decb516
mmh <114795041+unknownpioneer@users.noreply.github.com>
2026-06-13 23:53:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -90,8 +90,9 @@ st.title("📚 Multi-Source English Reader (BBC + Inside Story)")
 
 sources = {
     "🇬🇧BBC News": "https://feeds.bbci.co.uk/news/rss.xml",
-    "🇬🇧Inside Story (Australia)": "https://insidestory.org.au/feed/",
+    "🇦🇺Inside Story (Australia)": "https://insidestory.org.au/feed/",
     "🇮🇹Republicca": "https://www.repubblica.it/rss/homepage/rss2.0.xml",
+    "🇬🇧The Guardian": "https://www.theguardian.com/world/rss",
 }
 
 selected_sources = []
