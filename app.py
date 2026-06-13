7e6004e5bde7b1cb81e200afb162490c7b40a630
mmh <114795041+unknownpioneer@users.noreply.github.com>
2026-06-13 23:48:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,8 @@ st.title("📚 Multi-Source English Reader (BBC + Inside Story)")
 sources = {
     "BBC News": "https://feeds.bbci.co.uk/news/rss.xml",
     "Inside Story (Australia)": "https://insidestory.org.au/feed/",
-    "Republicca": "https://www.repubblica.it/rss/homepage/rss2.0.xml"
+    "Republicca": "https://www.repubblica.it/rss/homepage/rss2.0.xml",
+    "Italian A1 (MeloLingua)": "https://melolingua.com/italian-texts-to-read/a1-beginner"
 }
 
 selected_sources = []
